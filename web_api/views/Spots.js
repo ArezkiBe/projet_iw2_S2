4f92aa460a0c33f0def79060ab3d3bfb43680b72
AMARA Bilal <amara.bilalpro@gmail.com>
2024-07-26 04:09:05 +0200

    js fini de la page spots

--- web_api/views/Spots.js
+++ web_api/views/Spots.js
@@ -3,15 +3,54 @@ import { Component } from "../core/MiniReact.js";
 import Button from "../components/Button.js";
 import MiniReactDom from "../core/MiniReactDom.js";
 import Banner from "../components/Banner.js";
+import Footer from "../components/Footer.js";
 
-
-class Spots extends Component{
+class Spots extends Component {
 
   constructor(props) {
     super();
     this.data = props.data;
+    const spots = this.data.getSpots();
+    this.state = {
+      spots: spots,
+      filterType: "All",
+    };
   }
 
+  setFilterType(type) {
+    if (this.state.filterType === type) {
+      this.state.filterType = "All";
+    } else {
+      this.state.filterType = type;
+    }
+    this.updateFilteredSpots();
+  }
+
+  updateFilteredSpots() {
+    const filterType = this.state.filterType;
+    const allSpots = this.data.getSpots();
+    let filteredSpots;
+
+    if (filterType === "All") {
+      filteredSpots = allSpots;
+    } else {
+      filteredSpots = allSpots.filter(spot => {
+        if (filterType === "Indoor" || filterType === "Outdoor") {
+          return spot.type.includes(filterType);
+        }
+        return spot.rating.toString() === filterType;
+      });
+    }
+
+    this.state.spots = filteredSpots;
+    this.update();
+  }
+
+  update() {
+    const element = this.render();
+    const root = document.getElementById('root');
+    root.replaceChild(MiniReactDom.renderStructure(element), root.firstChild);
+  }
 
   render() {
     return {
@@ -31,20 +70,20 @@ class Spots extends Component{
         {
           type: "div",
           attributes: {
-            class: "mt-[54px] lg:mt-[102.24px]"
+            class: "mt-[54px]"
           },
           children: [
             {
               type :"div",
               attributes : {
-                class: "text-center font-bold text-[2.5rem] lg:text-[56.8px]"
+                class: "text-center font-bold text-[2.5rem] font-title"
               },
               children: ["Spots"],
             },
             {
               type :"div",
               attributes : {
-                class: "mt-[24px] lg:mt-[56.8px] flex justify-center items-center text-center lg:text-[35.5px] px-[1.813rem]"
+                class: "mt-[24px] flex justify-center items-center text-center px-[1.813rem] font-texte"
               },
               children: [this.data.getMainPage().description],
             },
@@ -52,17 +91,26 @@ class Spots extends Component{
         },
         this.renderFilter(),
         this.renderSpots(),
+        { type: Footer },
       ],
     };
   }
 
-
-
   renderFilter () {
+    const filterButtons = [
+      { text: "Intérieur", filter: "Indoor" },
+      { text: "Extérieur", filter: "Outdoor" },
+      { text: "5 étoiles", filter: "5" },
+      { text: "4 étoiles", filter: "4" },
+      { text: "3 étoiles", filter: "3" },
+      { text: "2 étoiles", filter: "2" },
+      { text: "1 étoile", filter: "1" },
+    ];
+
     return {
       type: "div",
       attributes: {
-        class: "w-full overflow-x-auto no-scrollbar mt-[54px] lg:mt-[102.24px] mb-[24px]"
+        class: "w-full overflow-x-auto no-scrollbar mt-[54px] mb-[24px]"
       },
       children: [
         {
@@ -70,262 +118,171 @@ class Spots extends Component{
           attributes: {
             class: "flex space-x-2"
           },
-          children: [
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "Interieur"
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "Extérieur"
-                
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "5 étoiles"
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "4 étoiles"
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "3 étoiles"
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "2 étoiles"
-              }
-            },
-            {
-              type: Button,
-              attributes: {
-                class: "flex-shrink-0 w-24 h-7 bg-[#D9D9D9] lg:text-[35.5px] rounded-[0.625rem] lg:w-[248.5px] lg:h-[56.8px]"
-              },
-              events : {
-                click: () => ""
-              },
-              props : {
-                title: "1 étoile"
-              }
-            }
-          ]
+          children: filterButtons.map(button =>
+            this.renderButton(button.text, button.filter)
+          )
         }
       ]
     }
   }
 
-
-  renderSpots () {
+  renderButton(text, filterType) {
+    const isSelected = this.state.filterType === filterType;
     return {
       type: "div",
       attributes: {
-        class: "flex justify-center items-center border-t-2 ml-4 mr-[13px] lg:mt-[102.24px]"
+        class: `flex-shrink-0 w-24 h-7 ${isSelected ? "bg-blue-700" : "bg-blue-500"} text-white rounded-xl`,
       },
       children: [
         {
-          type: "div",
-          attributes: {
-            class: "px-2 pb-[26px] rounded-lg"
+          type: "button",
+          attributes: { class: "w-full h-full font-texte" },
+          events: {
+            click: () => this.setFilterType(filterType),
           },
-          children: [
-            {
-              type: "div",
-              attributes: {
-                class: "font-bold text-center lg:text-[45.44px] my-[10px]"
-              },
-              children: ["LA CONCORDE"]
+          children: [text],
+        },
+      ],
+    };
+  }
+
+  renderSpots () {
+    if (!this.state.spots || this.state.spots.length === 0) {
+      return {
+        type: "div",
+        attributes: {
+          class: "text-center mt-8 font-texte",
+        },
+        children: ["Aucun spot trouvé."]
+      };
+    }
+
+    return {
+      type: "div",
+      attributes: {
+        class: "flex flex-wrap justify-center"
+      },
+      children: this.state.spots.map(spot => ({
+        type: "div",
+        attributes: {
+          class: "flex justify-center items-center border-t-2 ml-4 mr-[13px]"
+        },
+        children: [
+          {
+            type: "div",
+            attributes: {
+              class: "px-2 pb-[26px] rounded-lg"
             },
-            {
-              type: "div",
-              attributes: {
-                class: "flex items-center bg-white space-x-20 lg:w-[937.2px] lg:h-[357.84px]"
+            children: [
+              {
+                type: "div",
+                attributes: {
+                  class: "font-bold text-center my-[10px] font-texte"
+                },
+                children: [spot.name]
               },
-              children: [
-                {
-                  type: "img",
-                  attributes: {
-                    src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/concorde.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                    alt: "La Concorde",
-                    class: "w-[102px] lg:w-[213px] h-[102px] lg:h-[213px] rounded-md"
-                  }
+              {
+                type: "div",
+                attributes: {
+                  class: "flex items-center bg-white"
                 },
-                {
-                  type: "div",
-                  attributes: {
-                    class: "ml-[16px]"
-                  },
-                  children: [
-                    {
-                      type: "div",
-                      attributes: {
-                        class: "font-semibold text-[11px] lg:text-[35.5px] opacity-50 text-center"
-                      },
-                      children: ["Place de la Madeleine 75008 Paris"]
-                    },
-                    {
-                      type: "div",
-                      attributes: {
-                        class: "flex justify-center items-center space-x-10 mt-[10px]"
-                      },
-                      children: [
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/basketBall.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[30px] lg:w-[78.1px] h-[30px] lg:h-[84.49px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/archery.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[30px] lg:w-[78.1px] h-[30px] lg:h-[84.49px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/pingPong.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[30px] lg:w-[78.1px] h-[30px] lg:h-[84.49px]"
-                          }
-                        }
-                      ]
+                children: [
+                  {
+                    type: "img",
+                    attributes: {
+                      src: spot.logoUrl,
+                      alt: spot.name,
+                      class: "w-[102px] h-[102px] rounded-md"
                     }
-                  ]
-                },
-                {
-                  type: "div",
-                  attributes: {
-                    class: "border-l-2 border-gray-300 h-[100px] lg:h-[168.27px] ml-4 mr-2"
-                  }
-                },
-                {
-                  type: "div",
-                  attributes: {
-                    class: "flex flex-col justify-center items-center"
                   },
-                  children: [
-                    {
-                      type: Button,
-                      attributes: {
-                        class: "bg-red-500 text-white lg:text-[35.5px] lg:w-[213.71px] lg:h-[95.14px] px-4 py-2 rounded-[14.2px] cursor-pointer"
+                  {
+                    type: "div",
+                    attributes: {
+                      class: "ml-[16px]"
+                    },
+                    children: [
+                      {
+                        type: "div",
+                        attributes: {
+                          class: "font-semibold text-[11px] opacity-50 w-[119px] text-center"
+                        },
+                        children: [spot.address]
                       },
-                      events : {
-                        click: () => ""
-                      },
-                      props : {
-                        title: "DETAILS"
+                      {
+                        type: "div",
+                        attributes: {
+                          class: "flex justify-center items-center space-x-2 mt-[10px]"
+                        },
+                        children: spot.type.map(type => ({
+                          type: "img",
+                          attributes: {
+                            src: type.logoUrl,
+                            alt: type.name,
+                            class: "w-[30px] h-[30px]"
+                          }
+                        }))
                       }
-                    },
-                    {
-                      type: "div",
-                      attributes: {
-                        class: "flex space-x-[5px] mt-2"
-                      },
-                      children: [
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/full_star.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[15px] lg:w-[35.5px] h-[14px] lg:h-[42.6px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/full_star.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[15px] lg:w-[35.5px] h-[14px] lg:h-[42.6px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/empty_star.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[15px] lg:w-[35.5px] h-[14px] lg:h-[42.6px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/empty_star.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[15px] lg:w-[35.5px] h-[14px] lg:h-[42.6px]"
-                          }
-                        },
-                        {
-                          type: "img",
-                          attributes: {
-                            src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/empty_star.svg?alt=media&token=7c86bdb2-709b-4b6c-a42d-aabd8e9f0662",
-                            alt: "",
-                            class: "w-[15px] lg:w-[35.5px] h-[14px] lg:h-[42.6px]"
-                          }
-                        }
-                      ]
+                    ]
+                  },
+                  {
+                    type: "div",
+                    attributes: {
+                      class: "border-l-2 border-gray-300 h-[100px] ml-4 mr-2"
                     }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      ]
+                  },
+                  {
+                    type: "div",
+                    attributes: {
+                      class: "flex flex-col justify-center items-center"
+                    },
+                    children: [
+                      {
+                        type: Button,
+                        attributes: {
+                          class: "font-texte  bg-[#F0282D] text-[#FFB114] px-4 py-2 rounded-full cursor-pointer"
+                        },
+                        events: {
+                          click: () => {
+                            window.location.href = `/spot?id=${spot.id}`;
+                          }
+                        },
+                        props : {
+                          title: "DETAILS"
+                        }
+                      },
+                      {
+                        type: "div",
+                        attributes: {
+                          class: "flex space-x-[5px] mt-2"
+                        },
+                        children: this.renderStars(spot.rating)
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }))
     }    
   }
-}
 
+  renderStars(rating) {
+    let children = [];
+
+    for (let i = 1; i <= 5; i++) {
+      children.push({
+        type: "img",
+        attributes: {
+          class: "w-[18px] h-[18px]",
+          src: "https://firebasestorage.googleapis.com/v0/b/iw2-s2.appspot.com/o/"+(rating >= i ? "full_star" : "empty_star")+".svg?alt=media&token=cd6484b6-975d-4956-adb3-d88b19c63b40",
+        },
+      });
+    }
+
+    return children;
+  }
+}
 
 export default Spots;
